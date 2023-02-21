131f640597f8ee78cf2d254b9688a4a2269547c5
Andrew <lvN7yHB*s_I7y2jJ@+ZrgI_a#Ie1L7*%>
2023-02-21 23:25:33 +1000

    add urls

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -2,8 +2,7 @@ from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
 router_v1 = DefaultRouter()
-router_v1.register('auth/signup/', PostViewSet)
 
 urlpatterns = [
-    path('v1/', include(router_v1.urls)),
+    #path('auth/', RegistrationApiView.asView()),
 ]

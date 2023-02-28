fe2159496dae9b4e77f658a00bf760bf0e79aea0
andrew <andrew@prvrzv.com>
2023-02-28 20:08:55 +1000

    add order by id for user model

--- api_yamdb/users/models.py
+++ api_yamdb/users/models.py
@@ -41,6 +41,7 @@ class CustomUser(AbstractUser):
     class Meta:
         verbose_name = "Пользователь"
         verbose_name_plural = "Пользователи"
+        ordering = ('-id',)
         constraints = [
             models.UniqueConstraint(
                 fields=['username', 'email'],
